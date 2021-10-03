06cf1d8e11a5c1aa62de55d9162a75e0aca8f888
gyeongmoo97 <mwe22600@gmail.com>
2021-10-03 09:49:06 +0900

    [Feat][Order] OrderRowDetailDto 추가

---
+++ src/main/java/com/team3/shopping/dto/OrderRowDetailDto.java
@@ -0,0 +1,14 @@
+package com.team3.shopping.dto;
+
+import lombok.Data;
+
+@Data
+public class OrderRowDetailDto {
+	
+	private String color_code;
+	private String product_detail_url1;
+	private String pbrand;
+	private String pname;
+	private String pprice;
+	
+}
