ab5cc9f59786c54c43f4ff8998a75ea2e2aee050
fxdmin <go.jminn@gmail.com>
2021-10-05 20:09:06 +0900

    [Feat][ProductDetail] 상품 상세화면 및 함께 코디된 상품 출력

--- src/main/java/com/team3/shopping/dto/ProductListDto.java
+++ src/main/java/com/team3/shopping/dto/ProductListDto.java
@@ -8,9 +8,11 @@ public class ProductListDto {
 	private String pname;
 	private String pprice;
 	private String pbrand;
+	private String pdetail;
 	private String color_code;
 	private String product_detail_url1;
 	private String product_detail_url2;
+	private String product_detail_url3;
 	private String color_chip_url;
 	private String size_code;
 }
