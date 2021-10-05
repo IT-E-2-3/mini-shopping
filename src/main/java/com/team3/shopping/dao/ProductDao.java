1bcfdf568a2f8fe1c018ea4be592f88519fb1496
fxdmin <go.jminn@gmail.com>
2021-10-05 09:07:32 +0900

    [Feat] 상품목록 구현 및 페이징 처리

--- src/main/java/com/team3/shopping/dao/ProductDao.java
+++ src/main/java/com/team3/shopping/dao/ProductDao.java
@@ -1,17 +1,20 @@
 package com.team3.shopping.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.team3.shopping.dto.CategoryDto;
-import com.team3.shopping.dto.ProductDto;
+import com.team3.shopping.dto.Pager;
+import com.team3.shopping.dto.ProductListDto;
 
 @Mapper
 public interface ProductDao {
 	public List<CategoryDto> getcategoryList(String cateCode);
 	public CategoryDto currentCategory(String cateCode);
 	public CategoryDto parentCategory(String cateCode);
-	public List<ProductDto> getproductList(int catecode);
-
+	public List<ProductListDto> getproductList(@Param("pager") Pager pager, @Param("cateCode") String catecode);
+	public int getCountRows(String cateCode);
 }
